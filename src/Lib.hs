3b1674d5df705ef0ac6f282e040f65ad236fad79
Deivid Vale <deividrodriguesvale@gmail.com>
2019-12-06 21:19:38 +0100

    New Definition of Terms.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,14 +3,15 @@ module Lib
     ) where
 
 import qualified Type.SType as TYPE
-import qualified Terms.ATerms as Terms
+import qualified Terms.ATerms as TERMS
 
 someFunc :: IO ()
-someFunc = print arrow
-
+someFunc = print myvar
 natType = TYPE.Base "nat"
 natList = TYPE.Base "natlist"
 arrow = TYPE.Arrow natList natList
 arrow2 = TYPE.Arrow (TYPE.Arrow arrow natType) natType
+
+myvar = TERMS.newVar "nome" natType
 
 test = TYPE.typeOrder natType
--- src/Terms/ATerms.hs
+++ src/Terms/ATerms.hs
@@ -1,17 +1,19 @@
-module Terms.ATerms
-    (
-      Term
-    ) where
-
+module Terms.ATerms where
     import qualified Type.SType as TYPE
 
-    -- data VName = VName String Int
+    -- data FSymbol = FArity String TYPE.Type
+    -- deriving (Eq, Show)
 
-    data Var = VCons String Int TYPE.Type
+    data Term = VarCons String Int TYPE.Type | AppCons Term Term
+        deriving (Eq, Show)
 
-    data FSymbol = FArity String TYPE.Type
+    -- | Instantiate a new variable with the initial index.
+    -- This is the eta-reduced version of the function, it returns a function
+    newVar :: String -> TYPE.Type -> Term
+    newVar name = VarCons name 1
 
-    data Term = Var | App FSymbol [Term]
-
-
-
+    -- | Rename all variables in a term by it's index.
+    renameVar :: Term -> Term
+    renameVar var = case var of
+        VarCons name i tp -> VarCons name (i + 1) tp
+        AppCons t1 t2 -> AppCons (renameVar t1) (renameVar t2)
