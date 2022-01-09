f7cdbbd91cc1e68fce51d82249b05229cc08fa0b
Deivid Vale <deividrodriguesvale@gmail.com>
2022-01-09 16:29:23 +0100

    Added definitions for the functions in the Terms typeclass.

--- src/Interface/Syntax/Terms.hs
+++ src/Interface/Syntax/Terms.hs
@@ -1,14 +1,13 @@
 {-|
-Module      : AFS Terms
-Description : Short description
+Module      : Terms
+Description : Interface for Terms
 Copyright   : (c) Deivid Vale, 2021
 License     : MIT
 Maintainer  : deividrodriguesvale@gmail.com
 Stability   : experimental
 Portability : POSIX
 
-Here is a longer description of this module, containing some
-commentary with @some markup@.
+Defines the interfacing for terms.
 -}
 
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -20,7 +19,12 @@ import qualified Data.Set as Set
 import qualified Data.IntSet as IntSet
 
 class Terms t where
-    -- | ´vars s´ returns the list of all free variables occuring in s.
+    -- | @vars s@ returns the list of all free variables occuring in s.
     vars :: (Eq v, Ord v) => t v f -> Set.Set v
+
+    -- | @isVars s@ returns @True@ if @s@ is a variable
+    -- and @False@, otherwise.
     isVar :: t v f -> Bool
+
+    -- | Injects a variable value 'v' into a @t v f@ type constructed using 'v'.
     returnVar :: v -> t v f
